48d9cad95325598e22a8944ab4e2f4ef4350374a
redstarht <185753202+redstarht@users.noreply.github.com>
2024-11-10 22:12:01 +0900

    "addmemo"

---
+++ SQL/CREATE TABLE station_name.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS m_index_nbr (
+            index_nbr INT NOT NULL,
+            station_name TEXT NOT NULL,
+            CONSTRAINT index_nbr_pk primary key(index_nbr))
+
---
+++ SQL/DELETE FROM t_weather_log.sql
@@ -0,0 +1,1 @@
+DELETE FROM m_index_nbr
